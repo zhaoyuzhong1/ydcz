4467260b9eab1965179d546a590c7bb7e7d934a3
jameszyz <d>
2019-02-01 09:51:44 +0800

    增加Carimg 类

---
+++ src/main/java/com/yd/dto/CarImg.java
@@ -0,0 +1,43 @@
+package com.yd.dto;
+
+/**
+ * Created by james on 2019/2/1.
+ */
+public class CarImg extends BaseDTO{
+    private Integer id;
+    private Integer carid;
+    private String imgname;
+    private String imgpath;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCarid() {
+        return carid;
+    }
+
+    public void setCarid(Integer carid) {
+        this.carid = carid;
+    }
+
+    public String getImgname() {
+        return imgname;
+    }
+
+    public void setImgname(String imgname) {
+        this.imgname = imgname;
+    }
+
+    public String getImgpath() {
+        return imgpath;
+    }
+
+    public void setImgpath(String imgpath) {
+        this.imgpath = imgpath;
+    }
+}
