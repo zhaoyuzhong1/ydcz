04444e4639a2d16030f09cd86dc2e106b5c42479
jameszyz <d>
2019-02-01 09:34:46 +0800

    增加shop 类

---
+++ src/main/java/com/yd/dto/Shop.java
@@ -0,0 +1,63 @@
+package com.yd.dto;
+
+import java.util.Date;
+
+/**
+ * Created by james on 2019/2/1.
+ */
+public class Shop extends BaseDTO {
+    private Integer id;
+    private String name;
+    private String linkman;
+    private String mobile;
+    private String city;
+    private Date cdate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLinkman() {
+        return linkman;
+    }
+
+    public void setLinkman(String linkman) {
+        this.linkman = linkman;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Date getCdate() {
+        return cdate;
+    }
+
+    public void setCdate(Date cdate) {
+        this.cdate = cdate;
+    }
+}
