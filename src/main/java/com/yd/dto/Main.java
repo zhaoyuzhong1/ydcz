57acc282f0279d949e992a2ea7e8a0c032b0c453
jameszyz <d>
2019-02-13 16:05:07 +0800

    增加总店信息表

---
+++ src/main/java/com/yd/dto/Main.java
@@ -0,0 +1,43 @@
+package com.yd.dto;
+
+/**
+ * Created by james on 2019/2/13.
+ */
+public class Main extends BaseDTO {
+    private Integer id;
+    private String name;
+    private String address;
+    private String mobile;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+}
